edced73cdd8bbd285a3412c74c5a589567e1d32d
Bucharry Ramadhiawan <you@example.com>
2016-08-22 11:40:38 +0700

    css login

--- resources/views/isi/t_index.blade.php
+++ resources/views/isi/t_index.blade.php
@@ -4,7 +4,7 @@
 		<meta charset="UTF-8">
 		<meta http-equiv="X-UA-Compatible" content="IE-edge">
 		<title> Festiware </title>
-		<link rel="stylesheet" type="text/css" href="{!! asset('../assets/css/ss.css') !!}">
+		<link rel="stylesheet" type="text/css" href="{!! asset('/assets/css/ss.css') !!}">
 	</head>
 	<body>
 		@yield('content')
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,22 +5,25 @@
 	<span> {{ Session::get('message')}} </span>
 @endif
 
-<form method="post" action="{{url('/login')}}">
-	{{csrf_field()}}
-	Username : @if($errors->has())
-		<br/>
-		<span>{!! $errors->first('username') !!}</span>
+<center>
+	<h1 class="title-text"> Festiware </h1>
+	<form method="post" action="{{url('/login')}}" class="form-login">
+		{{csrf_field()}}
+		Username : @if($errors->has())
+			<br/>
+			<span>{!! $errors->first('username') !!}</span>
+			<p></p>
+			@endif
+		<input type="text" name="username" placeholder="Username" class="form-control"> <br> <br>
+		Password : @if($errors->has())
+			<br/>
+			<span>{!! $errors->first('password') !!}</span>
+			<p></p>
+			@endif
+		<input type="password" name="password" placeholder="Password" class="form-control"><p></p>
+		<a href="{{ URL('/register') }}" class="link-reg">Daftar</a> <p></p> 
 		<p></p>
-		@endif
-	<input type="text" name="username" placeholder="Username" class="form-control"> <br> <br>
-	Password : @if($errors->has())
-		<br/>
-		<span>{!! $errors->first('password') !!}</span>
-		<p></p>
-		@endif
-	<input type="password" name="password" placeholder="Password" class="form-control"><br>
-	<a href="{{ URL('/register') }}">Daftar</a> <p></p> 
-	<p></p>
-	<input type="submit" value="Login" class="btn-danger"/>
-</form>
+		<input type="submit" value="Login" class="btn-danger"/>
+	</form>
+</center>
 @stop
